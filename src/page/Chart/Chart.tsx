9695516ba52e0cb836a1c22c8858d1375fa6415c
programmerasif <mdasifkhan3757@gmail.com>
2024-08-17 20:06:47 +0600

    everything done

--- src/page/Chart/Chart.tsx
+++ src/page/Chart/Chart.tsx
@@ -11,13 +11,13 @@ import {
 } from "recharts";
 
 const data = [
-  { name: "Page A", uv: 4000, pv: 2400, amt: 2400 },
-  { name: "Page B", uv: 3000, pv: 1398, amt: 2210 },
-  { name: "Page C", uv: 2000, pv: 9800, amt: 2290 },
-  { name: "Page D", uv: 2780, pv: 3908, amt: 2000 },
-  { name: "Page E", uv: 1890, pv: 4800, amt: 2181 },
-  { name: "Page F", uv: 2390, pv: 3800, amt: 2500 },
-  { name: "Page G", uv: 3490, pv: 4300, amt: 2100 },
+  { name: "11 August", Total: 4000, Active: 2400, amt: 2400 },
+  { name: "12 August", Total: 3000, Active: 1398, amt: 2210 },
+  { name: "13 August", Total: 2000, Active: 9800, amt: 2290 },
+  { name: "14 August", Total: 2780, Active: 3908, amt: 2000 },
+  { name: "15 August", Total: 1890, Active: 4800, amt: 2181 },
+  { name: "16 August", Total: 2390, Active: 3800, amt: 2500 },
+  // { name: "17 August", Total: 3490, Active: 4300, amt: 2100 },
 ];
 
 const Chart = () => {
@@ -83,8 +83,8 @@ const Chart = () => {
               <YAxis />
               <Tooltip />
               <Legend />
-              <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-              <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
+              <Line type="monotone" dataKey="Total" stroke="#8884d8" />
+              <Line type="monotone" dataKey="Active" stroke="#82ca9d" />
             </LineChart>
           </ResponsiveContainer>
         </div>
--- src/page/QuickAccess/QuickAccess.tsx
+++ src/page/QuickAccess/QuickAccess.tsx
@@ -1,48 +1,227 @@
 import signe from "../../assets/check-mark.png";
+
 const QuickAccess = () => {
-    const image = [1,2,3]
-    return (
-        <div className="px-2 md:px-8 xl:px-16 md:mt-20  mt-10 ">
-            <div className="mb-10 text-center">
+  const image = [
+    { image: "https://i.ibb.co/PhNnNYr/109-helpline.jpg", phone: "109" },
+    { image: "https://i.ibb.co/RjM5X7j/duduk.jpg", phone: "106" },
+    { image: "https://i.ibb.co/fQPHgWy/1090.png", phone: "1090" },
+    { image: "https://i.ibb.co/8mNhnFT/1098.png", phone: "1098" },
+    { image: "https://i.ibb.co/SsH2BwF/16122s.png", phone: "16122" },
+    { image: "https://i.ibb.co/TgrmNqF/BLF-Logo.png", phone: "16109" },
+    { image: "https://i.ibb.co/yXt99jz/muktiyuddher.png", phone: "16171" },
+    { image: "https://i.ibb.co/6vwVrmd/thotto-Shompacaron.jpg", phone: "333" },
+    { image: "https://i.ibb.co/SNg10hT/BTRC.jpg", phone: "100" },
+    { image: "https://i.ibb.co/YbdQv38/CPU.jpg", phone: "16575" },
+    { image: "https://i.ibb.co/Z8KqSnN/forgen.jpg", phone: "16135" },
+    { image: "https://i.ibb.co/hmB1WKK/helpline-333.jpg", phone: "333" },
+    { image: "https://i.ibb.co/XSw8L65/999.jpg", phone: "999" },
+    { image: "https://i.ibb.co/jV24Lvn/fire-service.jpg", phone: "16163" },
+  ];
+
+  return (
+    <div className="px-2 md:px-8 xl:px-16 md:mt-20 mt-10">
+      <div className="mb-10 text-center">
         <div className="flex justify-center items-center gap-2">
           <img src={signe} alt="check-mark" className="w-12 md:w-14" />
           <h2 className="text-xl md:text-2xl font-semibold text-gray-700">
-          Quick Information 
+            Quick Information
             <span className="text-[#4CAF50]"> Accessibility</span>
           </h2>
         </div>
         <p className="mt-3 text-sm md:text-base text-gray-600">
-        Ensure users can easily access key information with minimal effort. Design intuitive navigation and clear pathways to deliver the right content instantly.
+          Ensure users can easily access key information with minimal effort.
+          Design intuitive navigation and clear pathways to deliver the right
+          content instantly.
         </p>
       </div>
-            <div className="grid grid-cols-3">
-            <div>
-                <h5>important links</h5>
-                <a href="">Office of the President</a>
-                <a href="">Prime Minister's Office</a>
-                <a href="">Bangladesh Employees Welfare Board</a>
-                <a href="">National Skill Development Authority</a>
-                <a href="">Primary digital content</a>
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
+        {/* Important Links */}
+        <div>
+          <h5 className="font-bold pb-4 md:pb-6 text-[#4CAF50] underline underline-offset-2">Central e-services</h5>
+          <div className="flex justify-start items-center flex-col border-x-4 min-h-96 bg-gray-50">
+            <div className="flex flex-col gap-4 mt-10">
+            <a href="#" className="flex justify-start items-center gap-2">
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6 text-green-500"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M19.902 4.098a3.75 3.75 0 0 0-5.304 0l-4.5 4.5a3.75 3.75 0 0 0 1.035 6.037.75.75 0 0 1-.646 1.353 5.25 5.25 0 0 1-1.449-8.45l4.5-4.5a5.25 5.25 0 1 1 7.424 7.424l-1.757 1.757a.75.75 0 1 1-1.06-1.06l1.757-1.757a3.75 3.75 0 0 0 0-5.304Zm-7.389 4.267a.75.75 0 0 1 1-.353 5.25 5.25 0 0 1 1.449 8.45l-4.5 4.5a5.25 5.25 0 1 1-7.424-7.424l1.757-1.757a.75.75 0 1 1 1.06 1.06l-1.757 1.757a3.75 3.75 0 1 0 5.304 5.304l4.5-4.5a3.75 3.75 0 0 0-1.035-6.037.75.75 0 0 1-.354-1Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </span>
+                <span>Office of the President</span>
+              </a>
+              <a href="#" className="flex justify-start items-center gap-2">
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6 text-green-500"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M19.902 4.098a3.75 3.75 0 0 0-5.304 0l-4.5 4.5a3.75 3.75 0 0 0 1.035 6.037.75.75 0 0 1-.646 1.353 5.25 5.25 0 0 1-1.449-8.45l4.5-4.5a5.25 5.25 0 1 1 7.424 7.424l-1.757 1.757a.75.75 0 1 1-1.06-1.06l1.757-1.757a3.75 3.75 0 0 0 0-5.304Zm-7.389 4.267a.75.75 0 0 1 1-.353 5.25 5.25 0 0 1 1.449 8.45l-4.5 4.5a5.25 5.25 0 1 1-7.424-7.424l1.757-1.757a.75.75 0 1 1 1.06 1.06l-1.757 1.757a3.75 3.75 0 1 0 5.304 5.304l4.5-4.5a3.75 3.75 0 0 0-1.035-6.037.75.75 0 0 1-.354-1Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </span>
+                <span>Prime Minister's Office</span>
+              </a>
+              <a href="#" className="flex justify-start items-center gap-2">
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6 text-green-500"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M19.902 4.098a3.75 3.75 0 0 0-5.304 0l-4.5 4.5a3.75 3.75 0 0 0 1.035 6.037.75.75 0 0 1-.646 1.353 5.25 5.25 0 0 1-1.449-8.45l4.5-4.5a5.25 5.25 0 1 1 7.424 7.424l-1.757 1.757a.75.75 0 1 1-1.06-1.06l1.757-1.757a3.75 3.75 0 0 0 0-5.304Zm-7.389 4.267a.75.75 0 0 1 1-.353 5.25 5.25 0 0 1 1.449 8.45l-4.5 4.5a5.25 5.25 0 1 1-7.424-7.424l1.757-1.757a.75.75 0 1 1 1.06 1.06l-1.757 1.757a3.75 3.75 0 1 0 5.304 5.304l4.5-4.5a3.75 3.75 0 0 0-1.035-6.037.75.75 0 0 1-.354-1Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </span>
+                <span>Bangladesh Employees Welfare Board</span>
+              </a>
+              <a href="#" className="flex justify-start items-center gap-2">
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6 text-green-500"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M19.902 4.098a3.75 3.75 0 0 0-5.304 0l-4.5 4.5a3.75 3.75 0 0 0 1.035 6.037.75.75 0 0 1-.646 1.353 5.25 5.25 0 0 1-1.449-8.45l4.5-4.5a5.25 5.25 0 1 1 7.424 7.424l-1.757 1.757a.75.75 0 1 1-1.06-1.06l1.757-1.757a3.75 3.75 0 0 0 0-5.304Zm-7.389 4.267a.75.75 0 0 1 1-.353 5.25 5.25 0 0 1 1.449 8.45l-4.5 4.5a5.25 5.25 0 1 1-7.424-7.424l1.757-1.757a.75.75 0 1 1 1.06 1.06l-1.757 1.757a3.75 3.75 0 1 0 5.304 5.304l4.5-4.5a3.75 3.75 0 0 0-1.035-6.037.75.75 0 0 1-.354-1Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </span>
+                <span>National Skill Development Authority</span>
+              </a>
+              <a href="#" className="flex justify-start items-center gap-2">
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6 text-green-500"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M19.902 4.098a3.75 3.75 0 0 0-5.304 0l-4.5 4.5a3.75 3.75 0 0 0 1.035 6.037.75.75 0 0 1-.646 1.353 5.25 5.25 0 0 1-1.449-8.45l4.5-4.5a5.25 5.25 0 1 1 7.424 7.424l-1.757 1.757a.75.75 0 1 1-1.06-1.06l1.757-1.757a3.75 3.75 0 0 0 0-5.304Zm-7.389 4.267a.75.75 0 0 1 1-.353 5.25 5.25 0 0 1 1.449 8.45l-4.5 4.5a5.25 5.25 0 1 1-7.424-7.424l1.757-1.757a.75.75 0 1 1 1.06 1.06l-1.757 1.757a3.75 3.75 0 1 0 5.304 5.304l4.5-4.5a3.75 3.75 0 0 0-1.035-6.037.75.75 0 0 1-.354-1Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </span>
+                <span>Primary Digital Content</span>
+              </a>
             </div>
-            <div>
-                <h5>Internal e-services</h5>
-                <a href="">Web mail</a>
-                <a href="">Online recruitment system</a>
-                <a href="">Scholarships and ICT Innovation Grants</a>
-                <a href="">National Enterprise Architecture</a>
-                
-            <div>
-                <h5>Emergency hotline</h5>
-                <div className="grid grid-cols-4 justify-center items-center gap-2">
-                    {
-                        image.map(item => <img src="" alt="" key={item}/>)
-                    }
-                </div>
-            </div>
-            </div>
-            </div>
+          </div>
         </div>
-    );
+        {/* Emergency Hotline */}
+        <div>
+          <h5 className="font-bold pb-4 md:pb-6 text-[#4CAF50] underline underline-offset-2">Emergency Hotline</h5>
+          <div className="grid grid-cols-3 justify-center items-center gap-2 max-h-96 overflow-y-scroll">
+            {image.map((item) => (
+              <div
+                key={item?.phone}
+                className="flex justify-center items-center flex-col bg-[#def8df] py-2 gap-2 rounded-md"
+              >
+                <img src={item?.image} alt="" className="w-20 rounded-md" />
+                <div className="flex justify-center items-center gap-2">
+                  <span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="w-6 h-6 text-green-500"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </span>
+                  {item?.phone}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Internal e-Services */}
+        <div>
+          <h5 className="font-bold pb-4 md:pb-6 text-[#4CAF50] underline underline-offset-2">Internal e-Services</h5>
+          <div className="flex justify-start items-center flex-col border-x-4 min-h-96 bg-gray-50">
+            <div className="flex flex-col gap-4 mt-10">
+            <a href="#" className="flex justify-start items-center gap-2">
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6 text-green-500"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M19.902 4.098a3.75 3.75 0 0 0-5.304 0l-4.5 4.5a3.75 3.75 0 0 0 1.035 6.037.75.75 0 0 1-.646 1.353 5.25 5.25 0 0 1-1.449-8.45l4.5-4.5a5.25 5.25 0 1 1 7.424 7.424l-1.757 1.757a.75.75 0 1 1-1.06-1.06l1.757-1.757a3.75 3.75 0 0 0 0-5.304Zm-7.389 4.267a.75.75 0 0 1 1-.353 5.25 5.25 0 0 1 1.449 8.45l-4.5 4.5a5.25 5.25 0 1 1-7.424-7.424l1.757-1.757a.75.75 0 1 1 1.06 1.06l-1.757 1.757a3.75 3.75 0 1 0 5.304 5.304l4.5-4.5a3.75 3.75 0 0 0-1.035-6.037.75.75 0 0 1-.354-1Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </span>
+                <span>Web Mail</span>
+              </a>
+              <a href="#" className="flex justify-start items-center gap-2">
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6 text-green-500"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M19.902 4.098a3.75 3.75 0 0 0-5.304 0l-4.5 4.5a3.75 3.75 0 0 0 1.035 6.037.75.75 0 0 1-.646 1.353 5.25 5.25 0 0 1-1.449-8.45l4.5-4.5a5.25 5.25 0 1 1 7.424 7.424l-1.757 1.757a.75.75 0 1 1-1.06-1.06l1.757-1.757a3.75 3.75 0 0 0 0-5.304Zm-7.389 4.267a.75.75 0 0 1 1-.353 5.25 5.25 0 0 1 1.449 8.45l-4.5 4.5a5.25 5.25 0 1 1-7.424-7.424l1.757-1.757a.75.75 0 1 1 1.06 1.06l-1.757 1.757a3.75 3.75 0 1 0 5.304 5.304l4.5-4.5a3.75 3.75 0 0 0-1.035-6.037.75.75 0 0 1-.354-1Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </span>
+                <span>Online Recruitment System</span>
+              </a>
+              <a href="#" className="flex justify-start items-center gap-2">
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6 text-green-500"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M19.902 4.098a3.75 3.75 0 0 0-5.304 0l-4.5 4.5a3.75 3.75 0 0 0 1.035 6.037.75.75 0 0 1-.646 1.353 5.25 5.25 0 0 1-1.449-8.45l4.5-4.5a5.25 5.25 0 1 1 7.424 7.424l-1.757 1.757a.75.75 0 1 1-1.06-1.06l1.757-1.757a3.75 3.75 0 0 0 0-5.304Zm-7.389 4.267a.75.75 0 0 1 1-.353 5.25 5.25 0 0 1 1.449 8.45l-4.5 4.5a5.25 5.25 0 1 1-7.424-7.424l1.757-1.757a.75.75 0 1 1 1.06 1.06l-1.757 1.757a3.75 3.75 0 1 0 5.304 5.304l4.5-4.5a3.75 3.75 0 0 0-1.035-6.037.75.75 0 0 1-.354-1Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </span>
+                <span>Ministry Dashboard</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default QuickAccess;
